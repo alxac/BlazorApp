6b231b9ceb693ee8fc8004589dd871c9a88659a4
Getinfo_Alexsander <alexsander@getinfo.tech>
2021-06-20 14:46:34 -0400

    Add Fatorial

---
+++ BlazorApp/Shared/FunctionUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Shared
+{
+    public static class FunctionUtil
+    {
+        public static int Fatorial(int numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else return numero * Fatorial(numero - 1);
+        }
+    }
+}
